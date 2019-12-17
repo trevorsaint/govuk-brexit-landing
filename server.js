b725790c068c67268d86b05920aa17b05bb3310b
trevorsaint <hello@trevorsaint.uk>
2019-12-17 09:29:04 +0000

    Added components. Updated footer to more closely match GOV.UK

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ middleware.forEach(func => app.use(func))
 var appViews = extensions.getAppViews([
   path.join(__dirname, '/app/views/'),
   path.join(__dirname, '/lib/'),
+  path.join(__dirname, '/src/'),
   path.join(__dirname, '/src/components/')
 ])
 
@@ -110,7 +111,8 @@ utils.addNunjucksFilters(nunjucksAppEnv)
 app.set('view engine', 'html')
 
 // Middleware to serve static assets
-app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/assets', express.static(path.join(__dirname, '/src/')));
 
 // Serve govuk-frontend in from node_modules (so not to break pre-extenstions prototype kits)
 app.use('/node_modules/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend')))
